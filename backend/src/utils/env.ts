08b7c0db1690718d60ddb31b48c983b014b32e32
Yuriy Pyrih <yuriypyrih@gmail.com>
2024-06-06 20:20:32 +0200

    fix: docker-compose production

--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -4,9 +4,9 @@ dotenv.config();
 
 export const env = {
   // (Required) Generic
-  FRONTEND_URL: process.env.FRONTEND_URL || 'NULL',
   NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT || 3005,
+  FRONTEND_URL: process.env.FRONTEND_URL || 'NULL',
+  PORT: process.env.PORT || 5000,
   MANAGER_EMAIL: process.env.MANAGER_EMAIL || '',
   SERVER_EMAIL: process.env.SERVER_EMAIL || '',
   SERVER_EMAIL_PASS: process.env.SERVER_EMAIL_PASS || '',
@@ -14,7 +14,6 @@ export const env = {
   DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || '',
   JWT_KEY: process.env.JWT_KEY || '',
   JWT_SECRET: process.env.JWT_SECRET || '',
-  JWT_COOKIE_EXPIRES_IN: process.env.JWT_COOKIE_EXPIRES_IN || '',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '',
   CYPHER_KEY: process.env.CYPHER_KEY || ''
 };
